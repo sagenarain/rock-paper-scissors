a0c51776c90319109bb470a31a7e25ef7b6584be
Sophie Narain <158600601+sophienarain@users.noreply.github.com>
2024-07-12 17:45:11 +0100

    add if else function

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-let choices = ["rock", "paper", "scissors"];
 // generate computer choice
 function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
     let x = Math.floor(Math.random() * choices.length);
     return choices[x]
 }
+    
 //input player choice
 function getHumanChoice() {
     let playerChoice
     playerChoice = prompt("enter a choice")
-    if (playerChoice.toLowerCase() === 'rock') {
+    if (playerChoice.toLowerCase() == 'rock') {
         return 'rock'
-    } else if (playerChoice.toLowerCase() === 'paper') {
+    } else if (playerChoice.toLowerCase() == 'paper') {
         return 'paper'
-    } else if (playerChoice.toLowerCase() === 'scissors') {
+    } else if (playerChoice.toLowerCase() == 'scissors') {
         return 'scissors'
     } else {
         return 'please enter rock, paper or scissors'
@@ -22,11 +23,17 @@ function getHumanChoice() {
 let humanScore = 0
 let computerScore = 0
 //play a round
-function playRound(humanChoice, computerChoice) {
-   
-}
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+    if(humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'scissors' && computerChoice === 'paper' ) {
+        console.log('You win.')
+    }
+   else if(humanChoice === computerChoice) {
+            console.log('Tie')
+        }
+        else {
+                console.log('You lose.')
+                }
+            
+            
